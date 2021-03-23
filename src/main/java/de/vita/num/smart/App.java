4c7691dc06fc50b5ce4eaf9373bdff069a73a779
serefarikan <serefarikan@gmail.com>
2021-03-23 11:09:29 +0000

    initial commit for maven based project artefacts.

---
+++ src/main/java/de/vita/num/smart/App.java
@@ -0,0 +1,13 @@
+package de.vita.num.smart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
