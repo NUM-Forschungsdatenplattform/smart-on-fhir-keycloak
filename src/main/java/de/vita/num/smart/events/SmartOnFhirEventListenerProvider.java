e4aa114547fcd404e825b5b5154b2e5111303d79
serefarikan <serefarikan@gmail.com>
2021-03-29 21:50:36 +0100

    don't create fhir resource and ehr if user is not using a SOF app
    We need to check if the registration event handler is performing smart on fhir related tasks only if the registered user is registering for a smart on fhir app. this initial implementation checks for launch/patient scope. since this scope is allowed only for smart on fhir apps in our keycloak config, it ensures that it is a smart on fhir app user who is registering.

--- src/main/java/de/vita/num/smart/events/SmartOnFhirEventListenerProvider.java
+++ src/main/java/de/vita/num/smart/events/SmartOnFhirEventListenerProvider.java
@@ -1,21 +1,21 @@
 package de.vita.num.smart.events;
 
 import de.vita.num.smart.authentication.AccessTokenBuilder;
-import de.vita.num.smart.authentication.SmartPatientAppAuthenticator;
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
-import org.keycloak.models.KeycloakContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RealmProvider;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
 import java.util.UUID;
 
 import static de.vita.num.smart.authentication.SmartPatientAppAuthenticator.CLIENT_SESSION_NOTE_KEY;
+import static de.vita.num.smart.authentication.SmartPatientAppAuthenticator.SCOPE_LAUNCH_PATIENT;
 
 public class SmartOnFhirEventListenerProvider implements EventListenerProvider {
 
@@ -35,6 +35,16 @@ public class SmartOnFhirEventListenerProvider implements EventListenerProvider {
     public void onEvent(Event event) {
 
         if (EventType.REGISTER.equals(event.getType())) {
+            AuthenticationSessionModel authSession =
+                session
+                    .getContext()
+                    .getAuthenticationSession();
+
+            // don't proceed if this is not a smart on fhir app user registration
+            String scope = authSession.getClientNote(OIDCLoginProtocol.SCOPE_PARAM);
+            if (!scope.contains(SCOPE_LAUNCH_PATIENT))
+                return;
+
             RealmModel realm = this.model.getRealm(event.getRealmId());
             String keycloakUserId = event.getUserId();
 
@@ -47,8 +57,7 @@ public class SmartOnFhirEventListenerProvider implements EventListenerProvider {
              * and the first access token generated after registration.
              * this is not needed with kc 12.
              */
-            session.getContext()
-                .getAuthenticationSession()
+            authSession
                 .setClientNote(
                     CLIENT_SESSION_NOTE_KEY,
                     UUID.randomUUID().toString());
